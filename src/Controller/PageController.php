809dd0c3155006f5788761f64034938e0bf899f6
Mrbilal <bilal_itchir@Outlook.fr>
2024-01-19 10:09:57 +0100

    🔧 corriger(PageController.php) : ajouter une nouvelle route pour la fonction email() 🎉 fonctionnalité(SecurityController.php) : ajouter les contrôleurs de sécurité pour la connexion et la déconnexion

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,11 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    /**
+     * A function that serves as the entry point for the 'paris' route.
+     *
+     * @return Response the HTTP response object
+     */
     #[Route('/', name: 'paris')]
     public function index(): Response
     {
         return $this->render('page/index.html.twig', [
 
+        ]);
+    }
+
+    /**
+     * Renders the confirmation email template.
+     *
+     * @return Response
+     */
+    #[Route('/email', name: 'email')]
+    public function email(): Response
+    {
+        return $this->render('registration/confirmation_email.html.twig', [
+            'signedUrl' => 'https://example.com/signed-url',
         ]);
     }
 }
