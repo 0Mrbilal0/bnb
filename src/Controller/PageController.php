0c255235cef2a47f296c709d520c1d47a4fbfdea
Mrbilal <bilal_itchir@Outlook.fr>
2024-01-19 11:33:49 +0100

    🐛 corriger(PageController.php) : ajouter les méthodes HTTP GET pour chaque route ✨ fonctionnalité(PageController.php) : ajouter des routes pour les villes Las Vegas, Kyoto, Sydney et Hong Kong 🐛 corriger(city.html.twig) : utiliser la variable 'title' pour le titre de la page ✨ fonctionnalité(city.html.twig) : ajouter un sous-titre pour les villes Kyoto et Hong Kong

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -13,11 +13,46 @@ class PageController extends AbstractController
      *
      * @return Response the HTTP response object
      */
-    #[Route('/', name: 'paris')]
+    #[Route('/', name: 'paris', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('page/city.html.twig', [
-
+            'title' => 'Paris',
+            'background' => 'paris',
+        ]);
+    }
+    #[Route('/lasvegas', name: 'lasvegas', methods: ['GET'])]
+    public function lasvegas(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Las Vegas',
+            'background' => 'lasvegas',
+        ]);
+    }
+    #[Route('/kyoto', name: 'kyoto', methods: ['GET'])]
+    public function kyoto(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Kyoto',
+            'subtitle' => '京都市',
+            'background' => 'kyoto',
+        ]);
+    }
+    #[Route('/sydney', name: 'sydney', methods: ['GET'])]
+    public function sydney(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Sydney',
+            'background' => 'sydney',
+        ]);
+    }
+    #[Route('/hongkong', name: 'hongkong', methods: ['GET'])]
+    public function hongkong(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Hong Kong',
+            'subtitle' => '香港',
+            'background' => 'hongkong',
         ]);
     }
 
